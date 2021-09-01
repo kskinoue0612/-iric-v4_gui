d929978814bad3e631839f8a1f455bcd529cb149
kazutake28 <kazutake.asahi@river-link.co.jp>
2021-09-01 18:58:50 +0900

    update

--- go.bat
+++
@@ -1,6 +0,0 @@
-call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2019
-
-set PATH=%PATH%;%USERPROFILE%\iRIC_v4\guis\prepost\
-
-.\INSTALL\bin\solver.exe
-
--- make.bat
+++ make.bat
@@ -1,20 +1,26 @@
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2019
 
+rem ----------------------------------------------------------------------
+rem set solver name
+rem ----------------------------------------------------------------------
 set sname=solver.exe
 
-ifort src\iric.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
-ifort src\main.f90 /Qopenmp /nostandard-realloc-lhs /MD /c
-
-ifort *.obj lib\iriclib.lib -o%sname%
-
+rem ----------------------------------------------------------------------
+rem ifort compile
+rem ----------------------------------------------------------------------
+cd .\INSTALL\bin
+ifort ..\..\src\iric.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort ..\..\src\main.f90 /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort *.obj ..\..\lib\iriclib.lib -o %sname%
 del *.obj
 del *.mod
 
-move %sname% .\INSTALL\bin\
-
+rem ----------------------------------------------------------------------
+rem copy files into the iRIC folder
+rem ----------------------------------------------------------------------
 set x=%~p0
 for %%1 in ("%x:~0,-1%") do set folder=%%~nx1
 echo '%USERPROFILE%\iRIC_v4\private\solvers%folder%'
 mkdir %USERPROFILE%\iRIC_v4\private\solvers\%folder%
 echo 'copy solver files'
-copy /y .\INSTALL\bin\* %USERPROFILE%\iRIC_v4\private\solvers\%folder%
+copy /y .\* %USERPROFILE%\iRIC_v4\private\solvers\%folder%
